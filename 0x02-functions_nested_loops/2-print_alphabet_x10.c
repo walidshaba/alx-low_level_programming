b638c731c16b1d3541720ef946b0327d674bfead
Musa Shaba <musamohshaba@gmaile.com>
2023-06-20 23:36:03 +0000

    plenty mistakes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
-#include "main.c"
-/*
- * main - prints 10X of each character
- * Return: 0
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
+
 void print_alphabet_x10(void)
 {
 	int i;
