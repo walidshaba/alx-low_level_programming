679655d8a49bbdf853999a1e1afc92026b83d598
Musa Shaba <musamohshaba@gmaile.com>
2023-06-20 23:50:44 +0000

    Ok betty am sorry

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@ void print_alphabet_x10(void)
 {
 	int i;
 	char letter;
-	for (i = 0; i < 10 ; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (letter = 'a' ; letter <= 'z' ; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
