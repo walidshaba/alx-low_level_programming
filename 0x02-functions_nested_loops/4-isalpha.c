bd4f9f94bdf39a7b874357bd526af08742de00a9
Musa Shaba <musamohshaba@gmaile.com>
2023-06-21 00:08:06 +0000

    ok test

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * int _isalpha(int c) - returns 1 if a lower or upper case 
+ * i_isalphai - returns 1 if a lower or upper case 
  * Return: 0
  */
 int _isalpha(int c)
 {
-	if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ( ( c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
